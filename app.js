d6047dd49a993eeffdd3f45d23111db9697ae198
Muhammad Mudassir <m.mudassir2015@gmail.com>
2022-06-29 21:02:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -53,14 +53,17 @@ app.get("/saad", (req, res)=>{
 })
 
 
-// if(process.env.NODE_ENV=='production'){
-import * as path from 'path'
+import path from "path";
 
   app.get('/',(req,res)=>{
       app.use(express.static(path.resolve(__dirname,'client','build')))
       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
 // }
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve("client", "build", "index.html"));
+// });
 
 
 
